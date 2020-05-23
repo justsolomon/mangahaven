11f8a6842462f1c96dfae703bbe4a9b439ba2468
Solomon <gbolahanbalogun5@gmail.com>
2020-05-23 14:55:48 +0100

    Add swipeable functionality to manga details menu

--- src/containers/js/MangaPage.js
+++ src/containers/js/MangaPage.js
@@ -22,6 +22,7 @@ import {
   WhatsappShareButton, WhatsappIcon
 } from "react-share";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
+import SwipeableViews from 'react-swipeable-views';
 import '../css/MangaPage.css';
 
 class MangaPage extends React.Component {
@@ -32,8 +33,6 @@ class MangaPage extends React.Component {
 			chapters: [],
 			lastChapter: '',
 			genres: [],
-			info: true,
-			chapter: false,
 			status: ['Ongoing', 'Ongoing', 'Completed'],
 			imageUrl: '',
 			gottenData: false,
@@ -41,7 +40,8 @@ class MangaPage extends React.Component {
 			favorite: false,
 			disabled: true,
 			shareBox: false,
-			urlCopied: false
+			urlCopied: false,
+			menuIndex: 0
 		}
 	}
 
@@ -66,19 +66,9 @@ class MangaPage extends React.Component {
 			.catch(console.log)		
 	}
 
-	changeToInfo = () => {
-		this.setState({
-			info: true,
-			chapter: false
-		})
-	}
+	changeToInfo = () => this.setState({ menuIndex: 0 })
 
-	changeToChapters = () => {
-		this.setState({
-			info: false,
-			chapter: true
-		})
-	}
+	changeToChapters = () => this.setState({ menuIndex: 1 })
 
 	sortChapters = () => {
 		this.setState({
@@ -159,10 +149,16 @@ class MangaPage extends React.Component {
 		return result;
 	}
 
+	changeIndex = () => {
+		let { menuIndex } = this.state;
+		if (menuIndex === 0) this.setState({ menuIndex: 1 });
+		else this.setState({ menuIndex: 0 });
+	}
+
 	render() {	
 		const entities = new AllHtmlEntities();
 		const { artist, alias, author, created, description, title, last_chapter_date, status } = this.state.manga;
-		const { info, chapter, lastChapter } = this.state;
+		const { lastChapter, menuIndex } = this.state;
 		const url = window.location.href;
 		const shareDescription = `Read ${title} online for free on MangaHaven`;
 
@@ -174,114 +170,132 @@ class MangaPage extends React.Component {
 								<BackButton toggleSearch={this.props.history.goBack} />
 								<p>{title}</p>
 							</div>
-							<FontAwesomeIcon onClick={this.displayShare} icon={faShareAlt} className={info ? 'active' : 'inactive'} />
-							<FontAwesomeIcon icon={faSort} className={chapter ? 'active' : 'inactive'} onClick={this.sortChapters} />
+							<FontAwesomeIcon onClick={this.displayShare} icon={faShareAlt} className={menuIndex === 0 ? 'active' : 'inactive'} />
+							<FontAwesomeIcon icon={faSort} className={menuIndex === 1 ? 'active' : 'inactive'} onClick={this.sortChapters} />
 							<FontAwesomeIcon icon={faCommentDots} />
 						</div>
 						<div className='manga-details-nav'>
-							<p onClick={this.changeToInfo}>INFO</p>
-							<p onClick={this.changeToChapters}>CHAPTERS</p>
+							<p 
+								onClick={this.changeToInfo} 
+								className={menuIndex === 0 ? 'current-menu' : ''}
+							>
+								INFO
+							</p>
+							<p 
+								onClick={this.changeToChapters} 
+								className={menuIndex === 1 ? 'current-menu' : ''}
+							>
+								CHAPTERS
+							</p>
 						</div>
-						<div className='active-lines'>
-							<div className={info ? 'active' : 'inactive'}></div>
-							<div className={chapter ? 'active' : 'inactive'}></div>
-						</div>
+						<div className={menuIndex === 0 ? 'active-menu-line' : 'active-menu-line chapter'}></div>
 					</div>
 
 					<div className='manga-details'>
-						<div className={info ? 'active manga-info' : 'inactive manga-info'}>
-							<div className='manga-info-header'>
-								<div className='manga-image'>
-									<Image url={this.state.imageUrl} />
+						<SwipeableViews index={menuIndex} onChangeIndex={this.changeIndex}>
+							<div className='manga-info'>
+								<div className='manga-info-header'>
+									<div className='manga-image'>
+										<Image url={this.state.imageUrl} />
+									</div>
+
+									<div className='manga-info-details'>
+										<p className='manga-title'>{title}</p>
+
+										<p className='manga-author'>
+											Author:<span>{entities.decode(author)}</span>
+										</p>
+
+										<p className='manga-artist'>
+											Artist:<span>{entities.decode(artist)}</span>
+										</p>
+
+										<p className='manga-created'>
+											Created:
+											<span>
+												{
+													this.state.gottenData ? 
+													new Date(created * 1000).toLocaleDateString() : null
+												}
+											</span>
+										</p>
+
+										<p className='manga-last-chapter'>
+											Last chapter:<span>{lastChapter}</span>
+										</p>
+
+										<p className='manga-last-updated'>
+											Updated:
+											<span>
+												{
+													this.state.gottenData ? 
+													new Date(last_chapter_date * 1000).toLocaleDateString() : null
+												}
+											</span>
+										</p>
+
+										<p className='manga-status'>
+											Status:<span>{this.state.status[status]}</span>
+										</p>
+									
+									</div>
 								</div>
 
-								<div className='manga-info-details'>
-									<p className='manga-title'>{title}</p>
-
-									<p className='manga-author'>
-										Author:<span>{entities.decode(author)}</span>
-									</p>
-
-									<p className='manga-artist'>
-										Artist:<span>{entities.decode(artist)}</span>
-									</p>
-
-									<p className='manga-created'>
-										Created:
-										<span>
-											{
-												this.state.gottenData ? 
-												new Date(created * 1000).toLocaleDateString() : null
-											}
-										</span>
-									</p>
-
-									<p className='manga-last-chapter'>
-										Last chapter:<span>{lastChapter}</span>
-									</p>
-
-									<p className='manga-last-updated'>
-										Updated:
-										<span>
-											{
-												this.state.gottenData ? 
-												new Date(last_chapter_date * 1000).toLocaleDateString() : null
-											}
-										</span>
-									</p>
-
-									<p className='manga-status'>
-										Status:<span>{this.state.status[status]}</span>
-									</p>
-								
-								</div>
-							</div>
-
-							<div className='action-icon-buttons'>
-								<button 
-									className='bookmark-icon' 
-									onClick={this.toggleBookmark} 
-									disabled={this.state.disabled}
-								>
-									{
-										!this.state.bookmark ? 
-										<FontAwesomeIcon icon={regularBookmark} /> : 
-										<FontAwesomeIcon icon={solidBookmark} />
-									}
-								</button>
-								<button 
-									className='heart-icon' 
-									onClick={this.toggleFav} 
-									disabled={this.state.disabled}
-								>
-									{
-										!this.state.favorite ? 
-										<FontAwesomeIcon icon={regularHeart} /> : 
-										<FontAwesomeIcon icon={solidHeart} />
-									}
-								</button>
-							</div>
-
-							<div className='manga-description'>
-								<div className='manga-genres'>
-									<p>Genres</p>
-									<div className='genre-links'>
+								<div className='action-icon-buttons'>
+									<button 
+										className='bookmark-icon' 
+										onClick={this.toggleBookmark} 
+										disabled={this.state.disabled}
+									>
 										{
-											this.state.genres.join(', ').split(' ').map((genre, i) => {
-												return <a href={`/genre/${genre.replace(',', '').toLowerCase()}`} key={i}>{genre}</a>
-											})
+											!this.state.bookmark ? 
+											<FontAwesomeIcon icon={regularBookmark} /> : 
+											<FontAwesomeIcon icon={solidBookmark} />
 										}
-									</div>
+									</button>
+									<button 
+										className='heart-icon' 
+										onClick={this.toggleFav} 
+										disabled={this.state.disabled}
+									>
+										{
+											!this.state.favorite ? 
+											<FontAwesomeIcon icon={regularHeart} /> : 
+											<FontAwesomeIcon icon={solidHeart} />
+										}
+									</button>
 								</div>
-								<div className='manga-description-details'>
-									<p>Description</p>
-									<div className='manga-description-note'>
-										<p>{this.formatDescription(entities.decode(description))}</p>
-										<p>{this.getNote(entities.decode(description))}</p>
+
+								<div className='manga-description'>
+									<div className='manga-genres'>
+										<p>Genres</p>
+										<div className='genre-links'>
+											{
+												this.state.genres.join(', ').split(' ').map((genre, i) => {
+													return <a href={`/genre/${genre.replace(',', '').toLowerCase()}`} key={i}>{genre}</a>
+												})
+											}
+										</div>
+									</div>
+									<div className='manga-description-details'>
+										<p>Description</p>
+										<div className='manga-description-note'>
+											<p>{this.formatDescription(entities.decode(description))}</p>
+											<p>{this.getNote(entities.decode(description))}</p>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
+
+
+							<div className='manga-chapters'>
+								<ChapterBoxList 
+									allChapters={this.state.chapters} 
+									mangaName={alias} 
+									mangaId={this.props.match.params.id} 
+								/>
+							</div>
+						</SwipeableViews>
 
 						<div 
 							className={this.state.shareBox ? 'active manga-share-box' : 'manga-share-box'} 
@@ -364,14 +378,6 @@ class MangaPage extends React.Component {
 
 						<div className={this.state.urlCopied ? 'active copy-success' : 'copy-success'}>
 							<span>Copied to clipboard</span>
-						</div>
-
-						<div className={chapter ? 'active manga-chapters' : 'inactive manga-chapters'}>
-							<ChapterBoxList 
-								allChapters={this.state.chapters} 
-								mangaName={alias} 
-								mangaId={this.props.match.params.id} 
-							/>
 						</div>
 					</div>
 				</div>
